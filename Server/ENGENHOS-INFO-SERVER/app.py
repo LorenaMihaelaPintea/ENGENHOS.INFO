c2e9a1bd1741d146a0999fb3c2a33ae1b7e3f8bf
LorenaMihaelaPintea <a2020161731@isec.pt>
2022-06-25 14:50:51 +0100

    Tuning Classifier
    To Do:
    -Image Augmentation
    -Results Screen
    -Finish Backend

--- Server/ENGENHOS-INFO-SERVER/app.py
+++ Server/ENGENHOS-INFO-SERVER/app.py
@@ -1,7 +1,11 @@
 import os
 
+import requests
+import tensorflow as tf
 from firebase_admin import credentials, firestore, initialize_app
 from flask import Flask, request, flash
+import numpy as np
+from keras.preprocessing import image
 
 app = Flask(__name__)
 
@@ -12,6 +16,23 @@ db = firestore.client()
 obj_ref = db.collection('objects')
 obj_save = db.collection('objectsFromUsers')
 
+classifier = tf.keras.models.load_model('model.h5')
+
+def labels(result):
+    if result[0][0] == 1:
+        return "AP Mine"
+    elif result[0][1] == 1:
+        return "AV Mine"
+    elif result[0][2] == 1:
+        return "Grenade"
+    elif result[0][3] == 1:
+        return "Mortar Round"
+    elif result[0][4] == 1:
+        return "Naval Ordnance"
+    elif result[0][5] == 1:
+        return "Projectile"
+    elif result[0][6] == 1:
+        return "Pyrotechnic or Flare"
 
 @app.route('/formImage', methods=['POST'])
 def post_formimage():
@@ -30,6 +51,10 @@ def post_formimage():
     if file:
         filename = file.filename
         file.save(os.path.join('./imagesForm', filename))
+        test_image = image.load_img(filename, target_size=(224, 224))
+        test_image = np.expand_dims(test_image, axis=0)
+        n_result = classifier.predict(test_image)
+        result = labels(n_result)
         return 200
 
 
@@ -50,6 +75,10 @@ def post_image():
     if file:
         filename = file.filename
         file.save(os.path.join('./images', filename))
+        test_image = image.load_img(filename, target_size=(224, 224))
+        test_image = np.expand_dims(test_image, axis=0)
+        n_result = classifier.predict(test_image)
+        result = labels(n_result)
         return 200
 
 
--- Server/ENGENHOS-INFO-SERVER/model.py
+++ Server/ENGENHOS-INFO-SERVER/model.py
@@ -1,97 +1,180 @@
+from keras.optimizer_v2.rmsprop import RMSprop
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
-from tensorflow.keras.optimizers import RMSprop, Adam, Adamax
 import numpy as np
 from keras.preprocessing import image
+from keras_tuner.tuners import RandomSearch
+# from keras_tuner.engine.hyperparameters import HyperParameters
+
+
+def build_model(hp):
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(hp.Int('input_units', min_value=1, max_value=32, step=1),
+                               (3, 3), padding='same', activation='relu', input_shape=(224, 224, 3)),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(hp.Int('conv_1_units', min_value=1, max_value=32, step=1),
+                               (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(hp.Int('conv_2_units', min_value=1, max_value=64, step=1),
+                               (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(hp.Int('conv_3_units', min_value=1, max_value=128, step=1),
+                               (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(hp.Int('conv_4_units', min_value=1, max_value=128, step=1),
+                               (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(hp.Int('conv_5_units', min_value=1, max_value=128, step=1),
+                               (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(hp.Int('conv_6_units', min_value=1, max_value=128, step=1),
+                               (3, 3), padding='same', activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(hp.Int('conv_7_units', min_value=1, max_value=256, step=1),
+                              activation='relu'),
+        tf.keras.layers.Dense(7, activation='softmax')
+    ])
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=RMSprop(learning_rate=0.001),
+                  metrics=['accuracy'])
+
+    return model
+
 
 batch_size = 32
+epochs = 50
 
 # Image normalization (0-1)
-train_datagen = ImageDataGenerator(rescale=1.0/255.0)
+generator = ImageDataGenerator(rescale=1.0 / 255.0, validation_split=0.2)
 
 # Flow training images using train_datagen generator
-train_generator = train_datagen.flow_from_directory(
-        'dataset',
-        target_size=(300, 300),
-        batch_size=batch_size,
-        classes=['Projectile', 'Grenade', 'AP Mine', 'Mortar Round', 'Pyrotechnic or Flare'],
-        class_mode='categorical')
+train_generator = generator.flow_from_directory(
+    'dataset',
+    target_size=(224, 224),
+    batch_size=batch_size,
+    classes=['AP Mine', 'AV Mine', 'Grenade', 'Mortar Round', 'Naval Ordnance', 'Projectile',
+             'Pyrotechnic or Flare'],
+    class_mode='categorical', subset='training', shuffle=True)
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(300, 300, 3)),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(5, activation='softmax')
-])
+validation_generator = generator.flow_from_directory(
+    'dataset',
+    target_size=(224, 224),
+    batch_size=batch_size,
+    classes=['AP Mine', 'AV Mine', 'Grenade', 'Mortar Round', 'Naval Ordnance', 'Projectile',
+             'Pyrotechnic or Flare'],
+    class_mode='categorical', subset='validation', shuffle=True)
 
-model.summary()
 
-model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(learning_rate=0.001),
-              metrics=['acc'])
+tuner = RandomSearch(build_model, objective='val_accuracy', max_trials=100)
+tuner.search(train_generator, epochs=epochs, batch_size=batch_size, validation_data=validation_generator)
 
-# model.compile(loss='categorical_crossentropy',
-#               optimizer=Adam(learning_rate=0.001),
-#               metrics=['acc'])
-
-# model.compile(loss='categorical_crossentropy',
-#               optimizer=Adamax(learning_rate=0.001),
-#               metrics=['acc'])
-#
+tuner.results_summary()
+print(tuner.get_best_hyperparameters()[0].values)
 
 total_sample = train_generator.n
 
-n_epochs = 25
+best_model = tuner.get_best_models()[0]
 
-history = model.fit(
+history = best_model.fit(
         train_generator,
+        validation_data=validation_generator,
         steps_per_epoch=int(total_sample/batch_size),
-        epochs=n_epochs,
+        epochs=epochs,
         verbose=1)
 
-# model.save('model.h5')
+best_model.save('model.h5')
 
-test_image_ap = image.load_img(path='AP Mine - 1.jpg', target_size=(300, 300))
-test_image_g = image.load_img(path='grenade-1.jpeg', target_size=(300, 300))
-test_image_p = image.load_img(path='projectile-1.jpg', target_size=(300, 300))
-test_image_pof = image.load_img(path='Pyrotechnic or Flare - 1.jpg', target_size=(300, 300))
-test_image_mr = image.load_img(path='Mortar Round - 1.jpg', target_size=(300, 300))
-
-test_image_p = np.expand_dims(test_image_p, axis=0)
-result_p = model.predict(test_image_p)
-print("Projectile")
-print(result_p)
-print("----------------------------------------")
-
-test_image_g = np.expand_dims(test_image_g, axis=0)
-result_g = model.predict(test_image_g)
-print("Grenade")
-print(result_g)
-print("----------------------------------------")
+test_image_ap = image.load_img(path='AP Mine - 1.jpg', target_size=(224, 224))
+test_image_ap1 = image.load_img(path='AP Mine - 2.jpg', target_size=(224, 224))
+test_image_av = image.load_img(path='AV Mine - 1.jpg', target_size=(224, 224))
+test_image_av1 = image.load_img(path='AV Mine - 2.jpg', target_size=(224, 224))
+test_image_g = image.load_img(path='grenade-1.jpeg', target_size=(224, 224))
+test_image_g1 = image.load_img(path='grenade-2.jpg', target_size=(224, 224))
+test_image_mr = image.load_img(path='Mortar Round - 1.jpg', target_size=(224, 224))
+test_image_mr1 = image.load_img(path='Mortar Round - 2.jpg', target_size=(224, 224))
+test_image_no = image.load_img(path='Naval Ordnance - 1.jpg', target_size=(224, 224))
+test_image_p = image.load_img(path='projectile-1.jpg', target_size=(224, 224))
+test_image_p1 = image.load_img(path='projectile-2.jpg', target_size=(224, 224))
+test_image_pof = image.load_img(path='Pyrotechnic or Flare - 1.jpg', target_size=(224, 224))
+test_image_pof1 = image.load_img(path='Pyrotechnic or Flare - 2.jpg', target_size=(224, 224))
 
 test_image_ap = np.expand_dims(test_image_ap, axis=0)
-result_ap = model.predict(test_image_ap)
+result_ap = best_model.predict(test_image_ap)
 print("AP Mine")
 print(result_ap)
 print("----------------------------------------")
 
+test_image_ap1 = np.expand_dims(test_image_ap1, axis=0)
+result_ap1 = best_model.predict(test_image_ap1)
+print("AP Mine")
+print(result_ap1)
+print("----------------------------------------")
+
+test_image_av = np.expand_dims(test_image_av, axis=0)
+result_av = best_model.predict(test_image_av)
+print("AV Mine")
+print(result_av)
+print("----------------------------------------")
+
+test_image_av1 = np.expand_dims(test_image_av1, axis=0)
+result_av1 = best_model.predict(test_image_av1)
+print("AV Mine")
+print(result_av1)
+print("----------------------------------------")
+
+test_image_g = np.expand_dims(test_image_g, axis=0)
+result_g = best_model.predict(test_image_g)
+print("Grenade")
+print(result_g)
+print("----------------------------------------")
+
+test_image_g1 = np.expand_dims(test_image_g1, axis=0)
+result_g1 = best_model.predict(test_image_g1)
+print("Grenade")
+print(result_g1)
+print("----------------------------------------")
+
 test_image_mr = np.expand_dims(test_image_mr, axis=0)
-result_mr = model.predict(test_image_mr)
+result_mr = best_model.predict(test_image_mr)
 print("Mortar Round")
 print(result_mr)
 print("----------------------------------------")
 
+test_image_mr1 = np.expand_dims(test_image_mr1, axis=0)
+result_mr1 = best_model.predict(test_image_mr1)
+print("Mortar Round")
+print(result_mr1)
+print("----------------------------------------")
+
+test_image_no = np.expand_dims(test_image_no, axis=0)
+result_no = best_model.predict(test_image_no)
+print("Naval Ordnance")
+print(result_no)
+print("----------------------------------------")
+
+test_image_p = np.expand_dims(test_image_p, axis=0)
+result_p = best_model.predict(test_image_p)
+print("Projectile")
+print(result_p)
+print("----------------------------------------")
+
+test_image_p1 = np.expand_dims(test_image_p1, axis=0)
+result_p1 = best_model.predict(test_image_p1)
+print("Projectile")
+print(result_p1)
+print("----------------------------------------")
+
+
 test_image_pof = np.expand_dims(test_image_pof, axis=0)
-result_pof = model.predict(test_image_pof)
+result_pof = best_model.predict(test_image_pof)
 print("Pyrotechnic or Flare")
 print(result_pof)
+print("----------------------------------------")
+
+test_image_pof1 = np.expand_dims(test_image_pof1, axis=0)
+result_pof1 = best_model.predict(test_image_pof1)
+print("Pyrotechnic or Flare")
+print(result_pof1)
 print("----------------------------------------")
